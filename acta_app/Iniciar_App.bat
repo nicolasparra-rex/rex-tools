751fde1e91ac1796089d08b1dd0465a42bbd2593
nicolasparra-rex <nicolas.parra@visma.com>
2026-05-07 17:31:35 -0400

    Delete acta_app directory
    se agrega otro

--- acta_app/Iniciar_App.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Iniciando Acta de Implementacion REX+...
-echo.
-echo Cuando veas el mensaje "You can now view...", abre tu navegador en:
-echo http://localhost:8501
-echo.
-python -m streamlit run app.py --server.headless true
